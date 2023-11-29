9f7a6ea9d377273dde833ea82bf72e05de8ce0c9
czpfreedom <czp940811@sina.com>
2023-11-30 01:21:29 +0800

    20231130

---
+++ Attribute.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Attribute 
+{
+
+    private double strength; // 力量
+    private double armor; // 护甲，物防
+
+    private double magicPower; //法强
+    private double magicResist; // 法抗
+
+    private double endurance; // 耐久，影响生命值上限
+    private double fortunate; // 幸运，影响暴击率和被暴击率
+    private double agility; // 零钱，影响命中率和被命中率
+    private double speed; // 速度，影响角色的ATB值增加速度
+    private double move; // 移动，影响角色的行动能力
+
+    private int strengthInt;
+
+
+}
